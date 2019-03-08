860b49b6bcb6e6fca1d4edd6de1ef9f8c21fcf9d
warpthatdot <44891716+warpthatdot@users.noreply.github.com>
2019-03-08 13:17:46 +0700

    Update index.go

--- index.go
+++ index.go
@@ -1,64 +1,49 @@
 package main
 
 import (
-	"encoding/json"
-	"github.com/gorilla/mux"
-	"log"
+	"fmt"
 	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/s1s1ty/go-mysql-crud/driver"
+	ph "github.com/s1s1ty/go-mysql-crud/handler/http"
 )
 
-type Person struct {
-	ID        string   `json:"id,omitempty`
-	Firstname string   `json:"firstname,omitempty`
-	Lastname  string   `json:"lastname,omitempty"`
-	Address   *Address `json:"address,omitempty"`
-}
-type Address struct {
-	City  string `json:"city,omitempty"`
-	State string `json:"state,omitempty"`
-}
-
-var people []Person
-
-func GetPersonEndpoint(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	for _, item := range people {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
-			return
-		}
-	}
-	json.NewEncoder(w).Encode(&Person{})
-}
-func GetPeopleEndpoint(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(people)
-}
-func CreatePersonEndpoint(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	var person Person
-	_ = json.NewDecoder(r.Body).Decode(&person)
-	person.ID = params["id"]
-	people = append(people, person)
-	json.NewEncoder(w).Encode(people)
-}
-func DeletePersonEndpoint(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	for index, item := range people {
-		if item.ID == params["id"] {
-			people = append(people[:index], people[index+1:]...)
-			break
-		}
-		json.NewEncoder(w).Encode(people)
-	}
-}
-
 func main() {
-	router := mux.NewRouter()
-	people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
-	people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
-	router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")
-	router.HandleFunc("/people/{id}", GetPersonEndpoint).Methods("GET")
-	router.HandleFunc("/people/{id}", CreatePersonEndpoint).Methods("POST")
-	router.HandleFunc("/people/{id}", DeletePersonEndpoint).Methods("DELETE")
-	log.Fatal(http.ListenAndServe(":8000", router))
+	dbName := os.Getenv("DB_NAME")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+
+	connection, err := driver.ConnectSQL(dbHost, dbPort, "root", dbPass, dbName)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+
+	r := chi.NewRouter()
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Logger)
+
+	pHandler := ph.NewPostHandler(connection)
+	r.Route("/", func(rt chi.Router) {
+		rt.Mount("/posts", postRouter(pHandler))
+	})
+
+	fmt.Println("Server listen at :8005")
+	http.ListenAndServe(":8005", r)
+}
+
+// A completely separate router for posts routes
+func postRouter(pHandler *ph.Post) http.Handler {
+	r := chi.NewRouter()
+	r.Get("/", pHandler.Fetch)
+	r.Get("/{id:[0-9]+}", pHandler.GetByID)
+	r.Post("/", pHandler.Create)
+	r.Put("/{id:[0-9]+}", pHandler.Update)
+	r.Delete("/{id:[0-9]+}", pHandler.Delete)
+
+	return r
 }
